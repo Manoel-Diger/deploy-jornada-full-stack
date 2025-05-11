598d143139873463cfb54e01ef776a178e9cd0da
Manoel-Diger <armenteiromanoel@gmail.com>
2025-05-11 20:31:44 -0300

    atualização url

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // const { NODE_ENV } = process.env;
 // const URL = "http://localhost:3001/api";
-const URL = "https://deploy-jornada-full-stack.onrender.com";
+const URL = "https://deploy-jornada-full-stack-y0qn.onrender.com/";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
@@ -12,3 +12,4 @@ export const artistArray = responseArtists.data;
 export const songsArray = responseSongs.data;
 
 // console.log(responseArtists.data);
+
